3fb1df47e773e2e34759511bb995bf29452b891e
Skappyy <55299218+Skappyy@users.noreply.github.com>
2020-02-26 11:36:41 -0800

    Add files via upload

---
+++ databaseProject/Index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Login System</title>
+    <link rel="stylesheet" href="Reset.css" type="text/css">
+    <link rel="stylesheet" href="css.css" type="text/css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="main-wrapper">
+                <ul>
+                    <li><a href="Index.php">Home</a></li>
+                </ul>
+            <div class="nav-login">
+                <form>
+                    <input type="text" name="uid" placeholder="Username/email">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <button type="submit" name="submit">Login</button>
+                </form>
+                <a href="signup.php">Sign Up</a>
+            </div>
+            </div>
+        </nav>
+    </header>
+    <section>
+
+    </section>
+</body>
+</html>
