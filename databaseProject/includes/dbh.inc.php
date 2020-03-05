434d9f299f3fdc2c7d80ca83d41298fe5b2cbd1c
Skappyy <55299218+Skappyy@users.noreply.github.com>
2020-03-05 11:30:23 -0800

    Add files via upload

---
+++ databaseProject/includes/dbh.inc.php
@@ -0,0 +1,7 @@
+<?php
+    $dbServername = "localhost"
+    $dbUsername = "root"
+    $dbPassword = "Wind"
+    $dbName = "loginsystem"
+
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName)
--- databaseProject/signup.php
+++ databaseProject/signup.php
@@ -4,7 +4,7 @@ include_once 'header.php';
     <section class="main-container">
         <div class="main-wrapper">
             <h2>Sign Up</h2>
-            <form class="signup-form">
+            <form class="signup-form" action="includes/signup.inc.php" method="post">
                 <input type="text" name="first" placeholder="First Name">
                 <input type="text" name="last" placeholder="Last Name">
                 <input type="text" name="email" placeholder="Email">
